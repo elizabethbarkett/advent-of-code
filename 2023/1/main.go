f30d2e0e148211e970485d3917e36911c2ccfabc
elizabethbarkett <ebarkett92@gmail.com>
2023-12-01 21:32:20 -0500

    part 2 solution... wildly overcomplicated but works

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
 
 	file, err := os.Open("input.txt")
+
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -18,16 +20,26 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	var sumPart1 int
+	var sumPart2 int
+
 	for scanner.Scan() {
-		num, err := partOne(scanner.Text())
+		text := scanner.Text()
+		num, err := partOne(text)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 		sumPart1 += num
-	}
 
+		num, err = partTwo(text)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		sumPart2 += num
+	}
 	fmt.Println(sumPart1)
+	fmt.Println(sumPart2)
 }
 
 func partOne(text string) (int, error) {
@@ -47,56 +59,77 @@ func partOne(text string) (int, error) {
 	return strconv.Atoi(fd + ld)
 }
 
-// func partTwo(text string) (int, error) {
-// 	var current string
-// 	var key map[string]int
-// 	for _, c := range text {
-// 		n, err := strconv.Atoi(string(c))
+func partTwo(text string) (int, error) {
 
-// 		if err != nil {
-// 			letter := string(c)
-// 			// start checking for a string digit
-// 			if current == "" {
-// 				m, ok := digits[letter]
-// 				if !ok && current == "" {
-// 					continue
-// 				}
-// 				key = m
-// 			}
-// 			// check to see if current exists in target map
-// 			current = current + letter
-// 			v, ok := key[current]
-// 			if ok {
-// 				return v, nil
-// 			}
-// 			for k, v := range key {
+	var addends []int
+	for i, c := range text {
+		// if a number
+		n, err := strconv.Atoi(string(c))
+		if err != nil {
+			digit := isNumber(text[i:])
+			if digit != 0 {
+				addends = append(addends, digit)
+			}
+		} else {
+			addends = append(addends, n)
+		}
+	}
+	if len(addends) == 1 {
+		addends = append(addends, addends[0])
+	}
+	fd := strconv.Itoa(addends[0])
+	ld := strconv.Itoa(addends[len(addends)-1])
+	return strconv.Atoi(fd + ld)
+}
 
-// 			}
-// 		}
-// 	}
-// 	return 0, nil
-// }
+func isNumber(text string) int {
+	var current string
+	var key map[string]int
+	for _, k := range text {
+		_, err := strconv.Atoi(string(k))
+		if err != nil {
+			if current == "" {
+				m, ok := digits[string(k)]
+				if !ok {
+					current = ""
+					continue
+				}
+				key = m
+			}
+			current = current + string(k)
+			// does it exist?
+			for k, v := range key {
+				if strings.HasPrefix(current, k) {
+					return v
+				}
+			}
+		} else {
+			return 0
+		}
+	}
+	return 0
+}
 
-// var digits = map[string]map[string]int{
-// 	"e": {
-// 		"eight": 8,
-// 	},
-// 	"f": {
-// 		"four": 4,
-// 		"five": 5,
-// 	},
-// 	"n": {
-// 		"nine": 9,
-// 	},
-// 	"o": {
-// 		"one": 1,
-// 	},
-// 	"s": {
-// 		"six":   6,
-// 		"seven": 7,
-// 	},
-// 	"t": {
-// 		"two":   2,
-// 		"three": 3,
-// 	},
-// }
+var digits = map[string]map[string]int{
+	"e": {
+		"eight": 8,
+	},
+	"f": {
+		"four": 4,
+		"five": 5,
+	},
+	"n": {
+		"nine": 9,
+	},
+	"o": {
+		"one": 1,
+	},
+	"s": {
+		"six":   6,
+		"seven": 7,
+	},
+	"t": {
+		"two":   2,
+		"three": 3,
+	},
+}
